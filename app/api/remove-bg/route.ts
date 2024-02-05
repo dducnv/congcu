3df7c51db8045f5a16762bc6329814d2f2c77b3c
Nguyen Van Duc <ducnv0712@gmail.com>
2024-02-05 14:04:56 +0700

    Refactor remove-bg route to handle different paths

--- app/api/remove-bg/route.ts
+++ app/api/remove-bg/route.ts
@@ -1,27 +1,33 @@
-import { NextApiRequest } from "next/types";
+import { encryptData } from "@/core/utils";
 
-export async function POST(request: NextApiRequest) {
-  const body = await request.body;
-  try {
-    const { file } = body;
-    const url = `https://api.remove.bg/v1.0/removebg`;
-    const headers = {
-      "X-Api-Key": process.env.RM_BG_API_KEY as string,
-    };
-    const formData = new FormData();
-    formData.append("image_file_b64", file);
+export async function POST(request: Request) {
+  const { url } = request;
+  const { pathname } = new URL(url);
+  const path = pathname.split("/").filter((i) => i);
+  const body = await request.json();
+  if (path[1] === "remove-bg") {
+    try {
+      const { file } = body;
+      const url = `https://api.remove.bg/v1.0/removebg`;
+      const headers = {
+        "X-Api-Key": process.env.RM_BG_API_KEY as string,
+      };
+      const formData = new FormData();
+      formData.append("image_file_b64", file);
 
-    const response = await fetch(url, {
-      method: "POST",
-      headers,
-      body: formData,
-    });
+      const response = await fetch(url, {
+        method: "POST",
+        headers,
+        body: formData,
+      });
 
-    var responseBlob = await response.blob();
-    return new Response(responseBlob, {
-      status: 200,
-    });
-  } catch (e) {
-    return new Response("Hệ thống gián đoạn!", { status: 500 });
+      var responseBlob = await response.blob();
+      return new Response(responseBlob, {
+        status: 200,
+      });
+    } catch (e) {
+      return new Response("Hệ thống gián đoạn!", { status: 500 });
+    }
   }
+  return new Response("Not found", { status: 404 });
 }
